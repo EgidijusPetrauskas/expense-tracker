9e66a0d318d834f39d725f936277fe4553c56952
E.Petrauskas <egidijus04petrauskas@gmail.com>
2022-05-03 17:18:50 +0300

    (feat): init custom theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
 import App from './app';
+
+import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#C5C6C7',
+    },
+  },
+});
+
+export default theme;
