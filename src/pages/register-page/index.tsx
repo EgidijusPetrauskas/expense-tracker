a8ac6f3ac28bfd1bc67ee1476f26f3a75d975f7d
E.Petrauskas <egidijus04petrauskas@gmail.com>
2022-05-07 18:39:25 +0300

    (feat): register page style

--- src/pages/register-page/index.tsx
+++ src/pages/register-page/index.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
+import { Box } from '@mui/material';
+import StyledTextField from '../../components/custom-form/custom-form-styles';
+import CustomForm from '../../components/custom-form';
+
 const RegisterPage: React.FC = () => (
-  <div>RegisterPage</div>
+  <Box
+    component="main"
+    sx={{
+      width: 1,
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <CustomForm
+      buttonText="Register"
+    >
+      <StyledTextField
+        type="text"
+        label="New Username"
+        name="username"
+        fullWidth
+      />
+      <StyledTextField
+        type="password"
+        label="Password"
+        name="password"
+        fullWidth
+      />
+      <StyledTextField
+        type="password"
+        label="Repeat Password"
+        name="password"
+        fullWidth
+      />
+    </CustomForm>
+  </Box>
 );
 
 export default RegisterPage;
